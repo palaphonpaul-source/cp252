b3e645b6fc0c3fefa5aded2dc0a57044fa84a724
palaphon <palaphon.paul@g.swu.ac.th>
2026-09-16 12:26:59 +0700

    Update server.js

--- lab3git/server.js
+++ lab3git/server.js
@@ -2,13 +2,13 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
 // Create the server and define what it does
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end(“<h1> Hello SWU </h1>”);
 });
 
 // Start listening for requests
